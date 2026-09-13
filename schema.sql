d16f61c9e2e0a4557e3d438e40fc62152abcf805
LucasKoz <sr.lucas.2k21@gmail.com>
2026-09-13 04:00:31 +0000

    prueba

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE DATEBASE IF NOT EXISTS daytona_login.php; 
+// creo la bd con el nombre "daytona_login"
+USE daytona_login.php;
+
+// creamos las tablas
+
+// empleados que se registran o logean
+CREATE TABLE usuarios (
+
+);
+
+// auditoria de los accesos
+CREATE TABLE accesos (
+
+);
