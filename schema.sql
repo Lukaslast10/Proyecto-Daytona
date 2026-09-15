64000d886f4f2f6a954208aa9f7f83babf8accd0
LucasKoz <sr.lucas.2k21@gmail.com>
2026-09-15 19:13:08 +0000

    base_de_datos_avance

--- schema.sql
+++ schema.sql
@@ -1,15 +1,23 @@
 CREATE DATEBASE IF NOT EXISTS daytona_login.php; 
-// creo la bd con el nombre "daytona_login"
+-- creamos la bd con el nombre "daytona_login"
 USE daytona_login.php;
 
-// creamos las tablas
+-- creamos las tablas
 
-// empleados que se registran o logean
-CREATE TABLE usuarios (
-
+-- empleados que se registran o logean
+CREATE TABLE usuarios ( -- datos necesarios - id, usuario, clave, email, perfil
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    usuario VARCHAR(40) UNIQUE NOT NULL,
+    clave VARCHAR(200) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    perfil ENUM('usuario','administrador') NOT NULL DEFAULT 'usuario',
+    -- fecha_registro ?
 );
 
-// auditoria de los accesos
-CREATE TABLE accesos (
-
+-- auditoria de los accesos
+CREATE TABLE accesos ( -- datos necesarios - id, usuario, fecha
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(40) NOT NULL,
+    fecha_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- necesario porque sino sabemos cuando pasó no auditamos nada
+    resultado ENUM('existoso','fallido') NOT NULL
 );
